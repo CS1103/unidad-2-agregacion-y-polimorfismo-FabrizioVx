262d5cd95e1fa6a246c563d9121bde528aa79dee
Fabrizio Vz <fabrizio.vasquez@utec.edu.pe>
2019-09-18 14:18:09 -0500

    v1

--- Biblioteca.h
+++ Biblioteca.h
@@ -5,6 +5,20 @@
 #ifndef UNIDAD_2_AGREGACION_Y_POLIMORFISMO_FABRIZIOVX_BIBLIOTECA_H
 #define UNIDAD_2_AGREGACION_Y_POLIMORFISMO_FABRIZIOVX_BIBLIOTECA_H
 
+class Volumen{
+protected:
+    string nombre = NULL;
+    int numVol = 0;
+public:
+    Volumen(string nombre, int numVol):nombre(nombre),numVol(numVol){}
+    virtual void mostrar(){
+        cout<<"Nombre: "<<nombre<<endl;
+        cout<<"Numero de Volumen: "<<numVol<<endl;
+    }
+
+};
+
+
 class Biblioteca{
 
 };
