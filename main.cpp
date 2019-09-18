f150c77664b4b4bdffcc9eb181a9df0348cb273a
Fabrizio Vz <fabrizio.vasquez@utec.edu.pe>
2019-09-18 16:41:04 -0500

    v8

--- main.cpp
+++ main.cpp
@@ -9,10 +9,14 @@ using namespace std;
 int main(){
     auto *libro1 = new Libro("The Big Nine", 1, 3);
     auto *libro2 = new Libro("The Theory of Everthing",4,8);
-    auto *revista = new Revista("Morat XD", 3, 10);
+    auto *revista1 = new Revista("Morat XD", 3, 10);
+    auto *revista2 = new Revista("TuRevista.com", 2, 1);
     auto biblioteca = new Biblioteca;
     biblioteca->incluir(libro1);
     biblioteca->incluir(libro2);
+    biblioteca->incluir(revista1);
+    biblioteca->incluir(revista2);
+
     biblioteca->mostrarBiblioteca();
 
 
